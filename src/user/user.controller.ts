5adfc744242046e5b23a04cfb8ab9ec5c692319a
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-03-24 11:46:03 +0700

    p03

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { IUser } from './user.interface';
 
-
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
