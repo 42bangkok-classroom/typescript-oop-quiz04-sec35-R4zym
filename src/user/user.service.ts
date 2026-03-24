0fe6ff07128655a1d5c3640579ca9df70e714774
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-03-24 10:48:15 +0700

    p02 & p03

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,19 @@
 // import path from 'path/win32';
+import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 // import fs from 'fs';
-
+@Injectable()
 export class UserService {
   private readonly UserPath = path.resolve(__dirname, '../data/students.json');
 
   private readJsonFile(filePath: string): string[] {
     try {
       const data = fs.readFileSync(filePath, 'utf-8');
-      return JSON.parse(data);
+      return JSON.parse(data) as string[];
     } catch (error) {
       console.error('Error reading JSON file:', error);
-      return [];
+      return [] as string[];
     }
   }
 
