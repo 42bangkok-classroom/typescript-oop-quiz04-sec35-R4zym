4dc24d956f510340e4e58e4a3262df5021d88a14
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-03-24 18:36:36 +0700

    p05

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,19 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-    @IsString()
-    @IsNotEmpty({ message: 'firstname should not be empty' })
-    firstname: string;
-    
-    @IsString()
-    @IsNotEmpty({ message: 'lastname should not be empty' })
-    lastname: string;
+  @IsString()
+  @IsNotEmpty({ message: 'firstname should not be empty' })
+  firstname: string;
 
-    @IsEmail()
-    @IsNotEmpty({ message: 'email should not be empty' })
-    email: string;
+  @IsString()
+  @IsNotEmpty({ message: 'lastname should not be empty' })
+  lastname: string;
 
-    @IsString()
-    @IsNotEmpty()
-    username: string;
+  @IsEmail()
+  @IsNotEmpty({ message: 'email should not be empty' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
 }
