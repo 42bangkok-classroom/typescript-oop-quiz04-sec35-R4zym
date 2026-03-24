7b6446e1f2aab37f692c43d3c47ad5d7a5f7bf54
Yossaran Mankongdee <yossaran.p6.2.17.ccs@gmail.com>
2026-03-24 12:16:30 +0700

    p01

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from './user/user.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly userService: UserService) {}
 
   getHello(): string {
     return 'Welcome to NestJS!';
